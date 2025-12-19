705b1fe91a248602993e290c11c83220c6951fbb
EstebanPastrana <estebenpc1234ppp@gmail.com>
2025-12-19 16:24:02 -0600

    Epastrana, se inicia el proyecto

---
+++ InventoryClass/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
